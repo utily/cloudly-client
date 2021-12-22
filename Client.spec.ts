012b1b14df492bdf9d2d730d42082b57741d4481
Simon Mika <simonmika@users.noreply.github.com>
2021-12-22 13:55:46 +0100

    Switched to use http.Client. (#2)
    * Renaming.
    
    * Fixed tests.

---
+++ Client.spec.ts
@@ -0,0 +1,7 @@
+import { Client } from "./index"
+
+describe("Client", () => {
+	it("create", () => {
+		expect(Client.create("url", "key")).toMatchObject({ client: { key: "key", url: "url" } })
+	})
+})

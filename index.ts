f476a9b5f23ada6ef07aa894acfbcb63b1388539
Tobias Liljeblad <tobias@issuefab.com>
2023-06-12 12:00:40 +0200

    update erxports

--- index.ts
+++ index.ts
@@ -1,4 +1,1 @@
-import { Client } from "./Client"
-import { Collection } from "./Collection"
-
-export { Client, Collection }
+export * as rest from "./rest"
---
+++ rest.ts
@@ -0,0 +1,2 @@
+export { Client } from "./Client"
+export { Collection } from "./Collection"
